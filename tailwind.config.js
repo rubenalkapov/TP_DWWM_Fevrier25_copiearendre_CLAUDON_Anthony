c1d6feb68b638c8f90cd443f5866c40d22911131
rubenalkapov <anthony.claudon21170@gmail.com>
2024-09-02 19:01:03 +0200

    Ajout des tailles d'écrans custom et des variables css

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,6 +9,7 @@ module.exports = {
       backgroundColor: {
         "forest-green": "var(--forest-green)",
         "ocean-blue": "var(--ocean-blue)",
+        "hover-ocean-blue": "var(--hover-ocean-blue)",
       },
       colors: {
         "sand-text": "var(--sand-text)",
@@ -18,7 +19,37 @@ module.exports = {
         "normal-font": "var(--normal-font)"
       }
     },
+    screens: {
+      'iphonese': {
+        'raw': '(min-height: 665px)',
+      },
+      'xs': '320px',
+      // => @media (min-width: 320px) { ... }
+
+      '2s': '375px',
+      // => @media (min-width: 375px) { ... }
+
+      's': '425px',
+      // => @media (min-width: 425px) { ... }
+
+      'sm': '640px',
+      // => @media (min-width: 640px) { ... }
+
+      'md': '768px',
+      // => @media (min-width: 768px) { ... }
+
+      'lg': '1024px',
+      // => @media (min-width: 1024px) { ... }
+
+      'xl': '1280px',
+      // => @media (min-width: 1280px) { ... }
+
+      '2xl': '2440px',
+      // => @media (min-width: 2440px) { ... }
+    }
   },
-  plugins: [],
+  plugins: [
+    require("@tailwindcss/forms")
+  ],
 }
 

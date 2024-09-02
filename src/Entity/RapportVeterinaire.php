efcbe6487ae89999da38b14cf9c3d624e57b3a04
rubenalkapov <anthony.claudon21170@gmail.com>
2024-09-02 12:39:50 +0200

    Création de l'entité pour le rapport vétérinaire

---
+++ src/Entity/RapportVeterinaire.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\RapportVeterinaireRepository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: RapportVeterinaireRepository::class)]
+class RapportVeterinaire
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column]
+    private ?int $report_id = null;
+
+    #[ORM\Column(type: Types::DATE_MUTABLE)]
+    private ?\DateTimeInterface $date = null;
+
+    #[ORM\Column(length: 50)]
+    private ?string $detail = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getReportId(): ?int
+    {
+        return $this->report_id;
+    }
+
+    public function setReportId(int $report_id): static
+    {
+        $this->report_id = $report_id;
+
+        return $this;
+    }
+
+    public function getDate(): ?\DateTimeInterface
+    {
+        return $this->date;
+    }
+
+    public function setDate(\DateTimeInterface $date): static
+    {
+        $this->date = $date;
+
+        return $this;
+    }
+
+    public function getDetail(): ?string
+    {
+        return $this->detail;
+    }
+
+    public function setDetail(string $detail): static
+    {
+        $this->detail = $detail;
+
+        return $this;
+    }
+}

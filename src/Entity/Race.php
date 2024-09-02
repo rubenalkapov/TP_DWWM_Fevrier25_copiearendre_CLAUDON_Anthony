d541c27d446a5fa818187b574ea874abee4099db
rubenalkapov <anthony.claudon21170@gmail.com>
2024-09-02 12:40:58 +0200

    Création de l'entité Race

---
+++ src/Entity/Race.php
@@ -0,0 +1,93 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\RaceRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: RaceRepository::class)]
+class Race
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column]
+    private ?int $race_id = null;
+
+    #[ORM\Column(length: 50)]
+    private ?string $label = null;
+
+    /**
+     * @var Collection<int, Animal>
+     */
+    #[ORM\OneToMany(targetEntity: Animal::class, mappedBy: 'race')]
+    private Collection $animals;
+
+    public function __construct()
+    {
+        $this->animals = new ArrayCollection();
+    }
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getRaceId(): ?int
+    {
+        return $this->race_id;
+    }
+
+    public function setRaceId(int $race_id): static
+    {
+        $this->race_id = $race_id;
+
+        return $this;
+    }
+
+    public function getLabel(): ?string
+    {
+        return $this->label;
+    }
+
+    public function setLabel(string $label): static
+    {
+        $this->label = $label;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection<int, Animal>
+     */
+    public function getAnimals(): Collection
+    {
+        return $this->animals;
+    }
+
+    public function addAnimal(Animal $animal): static
+    {
+        if (!$this->animals->contains($animal)) {
+            $this->animals->add($animal);
+            $animal->setRace($this);
+        }
+
+        return $this;
+    }
+
+    public function removeAnimal(Animal $animal): static
+    {
+        if ($this->animals->removeElement($animal)) {
+            // set the owning side to null (unless already changed)
+            if ($animal->getRace() === $this) {
+                $animal->setRace(null);
+            }
+        }
+
+        return $this;
+    }
+}

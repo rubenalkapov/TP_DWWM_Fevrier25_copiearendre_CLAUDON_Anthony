2476f262469ec32abe5c5da4aff4e019ef3ece9e
rubenalkapov <anthony.claudon21170@gmail.com>
2024-09-04 08:01:12 +0200

    Création du formulaire

---
+++ src/Form/AvisFormType.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Avis;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class AvisFormType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('pseudo', TextType::class, [
+                'label' => "Nom d'utilisateur",
+                'attr' => ['class' => 'form-control'],
+            ])
+            ->add('comments', TextareaType::class, [
+                'label' => "Commentaire",
+                'attr' => ['class' => 'form-control'],
+            ]);
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Avis::class,
+        ]);
+    }
+}

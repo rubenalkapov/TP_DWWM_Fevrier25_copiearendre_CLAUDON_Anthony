9e87c7087f18973faa0c7831c4668022abb9cc3a
rubenalkapov <anthony.claudon21170@gmail.com>
2024-09-05 16:32:23 +0200

    Intégration des avis dans le controller

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controller;
 
+use App\Entity\Avis;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -9,8 +11,12 @@ use Symfony\Component\Routing\Attribute\Route;
 class HomeController extends AbstractController
 {
     #[Route('/', name: 'app_home')]
-    public function index(): Response
+    public function index(EntityManagerInterface $entityManager): Response
     {
-        return $this->render('home/index.html.twig');
+        $avis = $entityManager->getRepository(Avis::class)->findAll();
+
+        return $this->render('home/index.html.twig', [
+            'avis' => $avis,
+        ]);
     }
 }
